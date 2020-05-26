9d2f457611ce08363629c9986ffddaa0e55ac023
antonmry <anton@galiglobal.com>
2020-05-26 23:06:17 +0200

    Fix Avro generator

--- src/main/java/com/SensorData/ReadFromKafka.java
+++ src/main/java/com/SensorData/ReadFromKafka.java
@@ -9,6 +9,7 @@ import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.formats.avro.AvroDeserializationSchema;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -49,14 +50,12 @@ public class ReadFromKafka {
         props.setProperty("group.id","firstGroup");
         props.setProperty("schema.registry.url","schema-registry:8081");
 
-        // create a kafka Consumer
-        AvroDeserializationSchema schema = new AvroDeserializationSchema(SensorData.class);
 
         //FlinkKafkaConsumer<SensorData> kafkaconsumer = new FlinkKafkaConsumer<SensorData>("Sensor-Data", schema,props);
 
         //1st query : Mall Foot Traffic-2 hours, each customer will be identified by it's Mac address
 
-        DataStreamSource<SensorData> mallFootTraffic = env.addSource(new FlinkKafkaConsumer<SensorData>("Sensor-Data", schema,props)
+        DataStreamSource<SensorData> mallFootTraffic = env.addSource(new FlinkKafkaConsumer<>("Sensor-Data", AvroDeserializationSchema.forSpecific(SensorData.class), props)
                 .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<SensorData>() {
             @Override
             public long extractAscendingTimestamp(SensorData element) {
@@ -87,46 +86,3 @@ public class ReadFromKafka {
     }
 }
 
-class AvroDeserializationSchema<T> implements DeserializationSchema<T> {
-
-    private final Class<T> avroType;
-
-    private transient DatumReader<T> reader;
-    private transient BinaryDecoder decoder;
-
-    public AvroDeserializationSchema(Class<T> avroType) {
-        this.avroType = avroType;
-    }
-
-    @Override
-    public T deserialize(byte[] message) {
-        ensureInitialized();
-
-        try {
-            decoder = DecoderFactory.get().binaryDecoder(message, decoder);
-            return reader.read(null, decoder);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public boolean isEndOfStream(T nextElement) {
-        return false;
-    }
-
-    @Override
-    public TypeInformation<T> getProducedType() {
-        return TypeExtractor.getForClass(avroType);
-    }
-
-    private void ensureInitialized() {
-        if (reader == null) {
-            if (org.apache.avro.specific.SpecificRecordBase.class.isAssignableFrom(avroType)) {
-                reader = new SpecificDatumReader<T>(avroType);
-            } else {
-                reader = new ReflectDatumReader<T>(avroType);
-            }
-        }
-    }
-}
