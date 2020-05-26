e8ab3d53ea0058f3c77271df78cd7afab8e254c7
elabbassiwidad <56221209+elabbassiwidad@users.noreply.github.com>
2020-05-26 20:33:10 +0200

    fixing errors

--- src/main/java/com/SensorData/ReadFromKafka.java
+++ src/main/java/com/SensorData/ReadFromKafka.java
@@ -1,25 +1,29 @@
 package com.SensorData;
 
-import com.SensorData.Functions.Counting;
-import com.SensorData.Functions.FootTrafficCollector;
-import com.SensorData.Functions.FootTrafficSerializationSchema;
-import com.SensorData.Functions.WindowedFootTraffic;
-
-
+import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.reflect.ReflectDatumReader;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.formats.avro.AvroDeserializationSchema;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.flink.streaming.util.serialization.DeserializationSchema;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+import org.apache.flink.util.Collector;
 
+import java.util.Date;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 
 public class ReadFromKafka {
@@ -36,40 +40,91 @@ public class ReadFromKafka {
 
         // Define Kafka Properties
         Properties props = new Properties();
-        props.setProperty("bootstrap.servers","kafka1:19092,kafka2:29092");
+        props.setProperty("bootstrap.servers","kafka1:19092");
         props.setProperty("zookeeper.connect","localhost:32181");
         props.setProperty("group.id","firstGroup");
         props.setProperty("schema.registry.url","schema-registry:8081");
 
         // create a kafka Consumer
-        FlinkKafkaConsumer011<SensorData> kafkaConsumer = new FlinkKafkaConsumer011<>("SensorData", AvroDeserializationSchema.forSpecific(SensorData.class), props );
-        kafkaConsumer.assignTimestampsAndWatermarks(new AscendingTimestampExtractor<SensorData>() {
+        AvroDeserializationSchema schema = new AvroDeserializationSchema(SensorData.class);
+
+        FlinkKafkaConsumer<SensorData> kafkaconsumer = new FlinkKafkaConsumer<SensorData>("Sensor-Data", schema,props);
+
+        kafkaconsumer.assignTimestampsAndWatermarks(new AscendingTimestampExtractor<SensorData>() {
             @Override
             public long extractAscendingTimestamp(SensorData element) {
                 return element.getTimestamp();
             }
         });
-        kafkaConsumer.setStartFromEarliest();
-
 
         //1st query : Mall Foot Traffic-2 hours, each customer will be identified by it's Mac address
-        DataStream<SensorData> mallFootTraffic = env.addSource(kafkaConsumer);
-        DataStream<WindowedFootTraffic> outStream = mallFootTraffic
-                .keyBy(SensorData::getSensorUID)
-                .windowAll(TumblingEventTimeWindows.of(Time.seconds(72000)))
-                .aggregate(new Counting(), new FootTrafficCollector())
-                .name("Visitors Counter");
+
+        DataStream<SensorData> mallFootTraffic = env.addSource(kafkaconsumer);
+
+                mallFootTraffic.timeWindowAll(Time.hours(2))
+                .apply(new AllWindowFunction<SensorData, Tuple2<Date,Long>, TimeWindow>() {
+                    @Override
+                    public void apply(TimeWindow timeWindow, Iterable<SensorData> iterable, Collector<Tuple2<Date, Long>> collector) throws Exception {
+                        long count =0;
+                        for(SensorData visitor : iterable)
+                            count++;
+                        collector.collect(new Tuple2<>(new Date(timeWindow.getEnd()), count));
+                    }
+                });
+        mallFootTraffic.print();
+
+        // 2nd query : In-Mall Proximity Traffic- zone, very important indicator in a retail store, it gives information about under-performing areas
 
          // create a kafka sink
-        outStream.addSink(new FlinkKafkaProducer011<WindowedFootTraffic>(
-                parameterTool.getRequired("mallFootTraficHistory"),
-                new FootTrafficSerializationSchema("mallFootTrafficHistory"),
-                props))
-                .name("FootTraffic Sink");
+      FlinkKafkaProducer<Tuple2> kafkaproducer = new FlinkKafkaProducer<Tuple2>("mallFootTraficHistory",
+              new SimpleStringSchema()
+              , props);
+
         env.execute();
 
-        //2nd query : Foot Traffic pattern- zone, very important indicator in a retail store, it gives information about under-performing areas
+    }
+}
 
+class AvroDeserializationSchema<T> implements DeserializationSchema<T> {
 
+    private final Class<T> avroType;
+
+    private transient DatumReader<T> reader;
+    private transient BinaryDecoder decoder;
+
+    public AvroDeserializationSchema(Class<T> avroType) {
+        this.avroType = avroType;
+    }
+
+    @Override
+    public T deserialize(byte[] message) {
+        ensureInitialized();
+
+        try {
+            decoder = DecoderFactory.get().binaryDecoder(message, decoder);
+            return reader.read(null, decoder);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean isEndOfStream(T nextElement) {
+        return false;
+    }
+
+    @Override
+    public TypeInformation<T> getProducedType() {
+        return TypeExtractor.getForClass(avroType);
+    }
+
+    private void ensureInitialized() {
+        if (reader == null) {
+            if (org.apache.avro.specific.SpecificRecordBase.class.isAssignableFrom(avroType)) {
+                reader = new SpecificDatumReader<T>(avroType);
+            } else {
+                reader = new ReflectDatumReader<T>(avroType);
+            }
+        }
     }
 }
